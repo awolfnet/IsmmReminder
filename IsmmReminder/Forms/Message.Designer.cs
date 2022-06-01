2f1449d8b2f94103228a6cafaf24ff979db92398
Wolf <haiyang@awolf.net>
2022-06-01 15:00:16 +0800

    add message form

---
+++ IsmmReminder/Forms/Message.Designer.cs
@@ -0,0 +1,48 @@
+﻿
+namespace IsmmReminder.Forms
+{
+    partial class Message
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Message
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1060, 636);
+            this.Name = "Message";
+            this.Text = "Message";
+            this.Load += new System.EventHandler(this.Message_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ IsmmReminder/Forms/Message.cs
@@ -0,0 +1,33 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IsmmReminder.Forms
+{
+    public partial class Message : Form
+    {
+        public ChromiumWebBrowser mainBrowser = null;
+
+        public Message()
+        {
+            InitializeComponent();
+
+            mainBrowser = new ChromiumWebBrowser("https://web.whatsapp.com");
+            this.Controls.Add(mainBrowser);
+            mainBrowser.Dock = DockStyle.Fill;
+        }
+
+        private void Message_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
