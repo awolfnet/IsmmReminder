e93f667d46a8f47fdbb532d64675570428949962
Nicolas Wolf <haiyang@awolf.net>
2023-03-31 02:18:27 +0800

    add settings: fetch

--- IsmmReminder/Controller/Faults.cs
+++ IsmmReminder/Controller/Faults.cs
@@ -293,6 +293,12 @@ namespace IsmmReminder.Controller
             int DaysOffset = -14;
             int.TryParse(ConfigurationManager.AppSettings["DaysToFetch"], out DaysOffset);
 
+            int FetchStart = 0;
+            int.TryParse(ConfigurationManager.AppSettings["FetchStart"], out FetchStart);
+
+            int FetchLength = 500;
+            int.TryParse(ConfigurationManager.AppSettings["FetchLength"], out FetchLength);
+
             HTTP http = new HTTP();
             http.Cookies = _cookies;
             Uri uri = new Uri(@"https://ismm.sg/ce/datatable/faults?draw=2&columns%5B0%5D%5Bdata%5D=action&columns%5B0%5D%5Borderable%5D=false&columns%5B1%5D%5Bdata%5D=f_num&columns%5B2%5D%5Bdata%5D=trd&columns%5B3%5D%5Bdata%5D=type&columns%5B4%5D%5Bdata%5D=cat&columns%5B5%5D%5Bdata%5D=sev&columns%5B6%5D%5Bdata%5D=st&columns%5B7%5D%5Bdata%5D=bl&columns%5B8%5D%5Bdata%5D=fl&columns%5B9%5D%5Bdata%5D=rm&columns%5B10%5D%5Bdata%5D=cff&columns%5B11%5D%5Bdata%5D=ca&columns%5B12%5D%5Bdata%5D=resd&columns%5B13%5D%5Bdata%5D=svd&columns%5B14%5D%5Bdata%5D=rad&columns%5B15%5D%5Bdata%5D=wsd&columns%5B16%5D%5Bdata%5D=wcd&columns%5B17%5D%5Bdata%5D=wcun&columns%5B18%5D%5Bdata%5D=act_dot&columns%5B19%5D%5Bdata%5D=act_taken&columns%5B19%5D%5Bsearchable%5D=false&columns%5B20%5D%5Bdata%5D=otrd&columns%5B21%5D%5Bdata%5D=cosd&columns%5B22%5D%5Bdata%5D=ard&columns%5B23%5D%5Bdata%5D=f_num_ref&columns%5B24%5D%5Bdata%5D=eun&columns%5B25%5D%5Bdata%5D=rank&columns%5B26%5D%5Bdata%5D=euc&columns%5B27%5D%5Bdata%5D=sr&columns%5B28%5D%5Bdata%5D=cu&columns%5B29%5D%5Bdata%5D=rmk&order%5B0%5D%5Bcolumn%5D=1&order%5B0%5D%5Bdir%5D=desc&start=10&length=10&search%5Bvalue%5D=&stat=&ls%5B%5D=60&ls%5B%5D=59&ls%5B%5D=58&ls%5B%5D=83&ls%5B%5D=79&ls%5B%5D=81&lb=&lf=&sd=2022-03-03&ed=2022-05-31&cat=&tp=&cc_stat=&resp_t=&pub=&inca=&inty=&_=1653995731017");
@@ -302,8 +308,8 @@ namespace IsmmReminder.Controller
             query.Set("_", DateTime.UtcNow.ToUnixTimeSeconds().ToString());
             query.Set("sd", DateTime.UtcNow.AddDays(DaysOffset).ToString("yyyy-MM-dd"));
             query.Set("ed", DateTime.UtcNow.ToString("yyyy-MM-dd"));
-            query.Set("start", "0");
-            query.Set("length", "500");
+            query.Set("start", FetchStart.ToString());
+            query.Set("length", FetchLength.ToString());
 
 
             string json = http.Request($"{uri.Scheme}://{uri.Host}{uri.AbsolutePath}?{query}");
